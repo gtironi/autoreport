29b3c9734ec9921f8e0559f8f11f69b5e8a160f3
GUSTAVO TIRONI <B51083@fgv.edu.br>
2023-04-24 08:55:56 -0300

    Criado a pasta dados

--- cria.py
+++ cria.py
@@ -1,1 +1,7 @@
 class relatorio:
+    def __init__(self) -> None:
+        self.cabecalho=None
+    
+    def inicializacao(self):
+        with open('..dados/cabecalho.tex')
+            self.cabecalho = f.read()
