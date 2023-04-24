0ad1e06e5bde4a44a85cfcd075215aefe4f821f4
Gustavo Tironi <b51083@fgv.edu.br>
2023-04-24 09:09:41 -0300

    Alterado os metodos da classe relatorio

--- cria.py
+++ cria.py
@@ -1,7 +1,9 @@
 class relatorio:
-    def __init__(self) -> None:
-        self.cabecalho=None
+    def __init__(self, autor, titulo) -> None:
+        self.autor = autor
+        self.titulo = titulo
+        self.cabecalho = None
     
     def inicializacao(self):
-        with open('..dados/cabecalho.tex')
+        with open('..dados/cabecalho.tex') as f:
             self.cabecalho = f.read()
