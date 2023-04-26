bb344f0225630adf835b62d1a6171fae769ac77b
Gustavo Tironi <b51083@fgv.edu.br>
2023-04-26 08:28:33 -0300

    cria.py mudado de pasta, adicionado mais pastas

--- cria.py
+++
@@ -1,9 +0,0 @@
-class relatorio:
-    def __init__(self, autor, titulo) -> None:
-        self.autor = autor
-        self.titulo = titulo
-        self.cabecalho = None
-    
-    def inicializacao(self):
-        with open('..dados/cabecalho.tex') as f:
-            self.cabecalho = f.read()
