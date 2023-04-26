926242d84e555e46ed126dceb57e07a403c47a9c
Gustavo Tironi <b51083@fgv.edu.br>
2023-04-26 08:35:26 -0300

    Definida a classe TestCria

--- tests/relatorio/test_cria.py
+++ tests/relatorio/test_cria.py
@@ -1,4 +1,7 @@
 import unittest
 from relatorio.cria import relatorio
 
-
+class TestCria(unittest.TestCase):
+    def test_cria_relatori(self):
+        R = relatorio("Meu relatório", "Eu mesmo")
+        self.assertEqual(R.titulo, "Meu relatório")
