c7034860c8f42af4b1f55fdc76f6e69baf45d081
Gustavo Tironi <b51083@fgv.edu.br>
2023-04-28 08:32:09 -0300

    Organizado arquivo para o jinja

--- relatorio/cria.py
+++ relatorio/cria.py
@@ -1,16 +1,16 @@
-from jinja2 import Environment
+from jinja2 import Environment, PackageLoader
 import os
 
 class relatorio:
     template_dir = "dados" #os.pat.join("..", "/dados")
     def __init__(self, autor, titulo, template='cabeçalho.tex') -> None:
-        env = Environment()
+        env = Environment(loader=PackageLoader("relatorio"))
         self.template_file = os.path.join(self.template_dir, template)
         self.template = env.get_template(self.template_file)
         self.autor = autor
         self.titulo = titulo
         self.cabecalho = None
-        self.inicializa()
+        #self.inicializa()
     
     def inicializa(self):
         with open(os.path.join(self.template_dir,'..dados/cabecalho.tex')) as f:
