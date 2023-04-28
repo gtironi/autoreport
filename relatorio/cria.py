49ca4c43a0f51c9ecf5b9ffd882eb81835f2d6ce
Gustavo Tironi <b51083@fgv.edu.br>
2023-04-28 08:17:59 -0300

    Modificado a classe relatorio (environment)

--- relatorio/cria.py
+++ relatorio/cria.py
@@ -1,9 +1,12 @@
+from jinja2 import Environment
 import os
 
 class relatorio:
     template_dir = "dados" #os.pat.join("..", "/dados")
     def __init__(self, autor, titulo, template='cabeçalho.tex') -> None:
-        self.template = template
+        env = Environment()
+        self.template_file = os.path.join(self.template_dir, template)
+        self.template = env.get_template(self.template_file)
         self.autor = autor
         self.titulo = titulo
         self.cabecalho = None
